2ee8de16aca19af4335ba15bd834c55c6816747c
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2023-06-29 10:12:02 +0100

    commit

--- components/PagesEditor/PagesEditor.jsx
+++ components/PagesEditor/PagesEditor.jsx
@@ -5,11 +5,20 @@ import { useEffect, useState } from "react";
 import getAllDocumentNames from "@/api/firebase/database/getAllDocumentNames";
 import deleteDocument from '@/api/firebase/database/deleteDocument';
 import setData from '@/api/firebase/database/setData';
+import { PlusLg } from 'react-bootstrap-icons';
+import { toast } from 'react-hot-toast';
+import { CaretRightFill } from 'react-bootstrap-icons';
 
 export default function PagesEditor(props) {
   const [pages, setPages] = useState([]);
 
   const [pageName, setPageName] = useState('');
+
+  const [showPageEditor, setShowPageEditor] = useState(false);
+
+  const togglePageEditor = () => {  
+    setShowPageEditor(!showPageEditor);
+  }
 
   const fetchPageContent = async () => {
     const result = await getAllDocumentNames(props.id);
@@ -19,13 +28,21 @@ export default function PagesEditor(props) {
   useEffect(() => {
     fetchPageContent();
   }, [props.id]);
-
+  
   const deletePage = async (id, page) => {
-    await deleteDocument(id, page);
-    fetchPageContent();
+    const confirmed = confirm(`Are you sure you want to delete ${page}?`);
+    if (confirmed) {
+      await deleteDocument(id, page);
+      fetchPageContent();
+    }
   }
 
   const addPage = async () => {
+    if (pageName === '') {
+      toast.error('Page name cannot be empty');
+      return;
+    }
+
     let type = '';
     let content = {};
     switch (props.id) {
@@ -65,18 +82,24 @@ export default function PagesEditor(props) {
 
   return (
     <>
-     <div className='add-new-page'>
-        <input type="text" className="form-control" placeholder="Page name" value={pageName}
-          onChange={(e) => setPageName(e.target.value)} />
-        <button className="btn btn-primary" onClick={addPage}>Add new page</button>
+      <div className='page-editor-header' onClick={togglePageEditor}>
+        <span className={`dropdown-icon ${showPageEditor ? 'open' : 'closed'}`}><CaretRightFill /></span>
+        <span className='title'>{`${props.id} pages editor`} </span>
+        <span></span>
       </div>
-      <div className="pages-editor">
+
+      <div className={`pages-editor ${showPageEditor ? 'show' : 'hide'}`}>
         {pages?.map((page, index) => (
-          <div className={`page ${props.selectedUrl === props.id + '/' + page ? 'selected' : ''}`} key={index} onClick={() => setSelected(props.id + '/' + page)}>
+          <div className={`page ${props.selectedUrl === props.id + '/' + page ? 'selected' : ''} form-control`} key={index} onClick={() => setSelected(props.id + '/' + page)}>
             <span>{`${props.id}/${page}`}</span>
             <button className="btn btn-danger" onClick={() => deletePage(props.id, page)}>Delete</button>
           </div>
         ))}
+        <div className='add-new-page form-control'>
+          <input type="text" placeholder={`New ${props.id}`} value={pageName}
+            onChange={(e) => setPageName(e.target.value)} />
+          <button className="btn btn-primary" onClick={addPage}><PlusLg height={"1.5em"} width={'1.5em'} /></button>
+        </div>
       </div>
     </>
   );
--- components/editable/plugins/GalleryMenu/EditGalleryMenu.jsx
+++ components/editable/plugins/GalleryMenu/EditGalleryMenu.jsx
@@ -4,12 +4,23 @@ import Image from "next/image"
 import PagesEditor from "../../../PagesEditor/PagesEditor"
 import './galleryMenu.scss'
 import { useState } from "react"
+import FileManager from "@/components/fileManager/FileManager"
+import getFileUrl from "@/api/firebase/database/getFileUrl"
 
 export default function EditGalleryMenu(props) {
 
   const items = props.content.items
 
   const [selectedUrl, setSelectedUrl] = useState(0)
+
+  const [fileManagerOpen, setFileManagerOpen] = useState(false)
+
+  const [selectedImage, setSelectedImage] = useState('')
+
+  const onFileClick = async (path) => {
+   const url = await getFileUrl(path)
+    setSelectedImage(url)
+  }
 
   const updateContent = () => {
     props.setContent((prevContent) => ({
@@ -18,12 +29,22 @@ export default function EditGalleryMenu(props) {
     }))
   }
 
+  const closeFileManager = () => {
+    setFileManagerOpen(false)
+  }
+
+  const selectImage = () => {
+    setFileManagerOpen(true)
+  }
+
+
   return (
     <>
       <PagesEditor id='gallery' setSelectedUrl={setSelectedUrl} selectedUrl={selectedUrl}/>
       <div className="edit-gallery-menu">
+      <FileManager openState={fileManagerOpen} close={closeFileManager} onFileClick={onFileClick}/>
         {items.map((item) => (
-          <Image src={item.image} width={200} height={200} alt={item.image} />
+          <Image src={item.image} width={200} height={200} alt={item.image} onClick={selectImage}/>
         ))}
       </div>
     </>
